d903f8b12491a6e4d80482e0433d1d88acb5204f
tramp <f20m21s22@gmail.com>
2018-09-22 21:20:13 +0800

    add timeout closeChannels nonBlockingChannelOperations rangeOverChannels

---
+++ basic/closeChannels.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sned job", j)
+	}
+	time.Sleep(time.Second * 2)
+	close(jobs)
+	fmt.Println("send all jobs")
+	<-done
+}
